cb8c101683d62800a52dab9e0819db59f97fd2b9
burak behlül <burakyabgu@gmail.com>
2025-10-08 18:01:46 +0300

    feat: Add authories control function

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -4,10 +4,15 @@ export default {
   name: "roleDelete", 
   async execute(client, role) {
     try {
-        const manager = new Manager(client);
-        await manager.authority.info(role.guild, manager.audit.RoleDelete);
-        
-        console.log(`[Role deleted]: ${role.id}`);
+        const manager = new Manager(client, {
+			authority: {
+				action: role.guild
+			}
+		});
+        const info = await manager.authority.info(manager.audit.RoleDelete);
+        const x = await manager.authority.isAuthorities(info.executorId,  ["BanMembers", "ManageRoles"])
+        // console.log(`[Role deleted]: ${role.id}`);
+        console.log(`[IA]`, x);
     } catch (error) {
         console.error('Error handling role deletion:', error);
     }
--- managers/authorityManager.js
+++ managers/authorityManager.js
@@ -4,12 +4,33 @@ export default class AuthorityManager {
         this.client = client;
 		this.options = options
     }
-    async info(child, type){
+    async info(type){
 		
-        const log = await child.fetchAuditLogs({limit:1,type: type})
+		// use: child.guild
+        const log = await this.options.action.fetchAuditLogs({limit:1,type: type})
         const entry = log.entries.first();
         
         console.log("[x] ", entry)
         return entry
     }
+	
+    async isAuthorities(userId, authorities){
+		const action = this.options.action
+        let member = action.members.cache.get(userId)
+			console.log("MEMBER", member.permissions.toArray());
+		
+        const result = authorities.map((authority)=> {
+			console.log("authority ",authority)
+            const isHasAuthority = member.permissions.has(authority)
+			console.log("isHA", isHasAuthority)
+            if(isHasAuthority){
+                return true
+            }
+            return false
+        })
+
+		return result.includes(true) 
+    }
+
+
 }
