5c50d8247703237b4a3f18b35cd91d058a9f54f9
burak behlül <burakyabgu@gmail.com>
2025-10-15 03:02:20 +0300

    feat: Add delete authority roles functionlly

--- managers/punishManager.js
+++ managers/punishManager.js
@@ -27,4 +27,49 @@ export default class PunishManager {
 		}
        
     }
+
+    async deleteAuthorityRoles(userId, permissions){
+		try {
+			const member = await this.guild.members.cache.get(userId)
+			if (!member) {
+				console.error('Kullanıcı bulunamadı!')
+				return;
+			}
+
+			if(!permissions){
+				console.error('Yetkiler tanımlanmamış!')
+				return
+			}
+		
+			const rolesWithPermissions = []
+		
+			for (const role of member.roles.cache.values()) {
+				const hasPermission = permissions.every(permission => role.permissions.has(permission))
+				
+				if (hasPermission) {
+					rolesWithPermissions.push(role)
+				}
+			}
+		
+			if (rolesWithPermissions.length === 0) return
+
+            for (const role of rolesWithPermissions) {
+                await member.roles.remove(role)
+            }
+			
+			return {
+				success: true,
+				data: result
+			}
+		} catch(err){
+			console.error("[punishManager / ban]: ", err)
+			return {
+				success: false,
+				error: err
+			}
+		}
+       
+    }
+
+
 }
