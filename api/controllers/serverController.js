d9784ddfe8a5acb49fa9da911b91cb2e60dcabb8
burak behlül <burakyabgu@gmail.com>
2025-10-27 00:16:56 +0300

    feat: Add bot controller in remove authority config

--- api/controllers/serverController.js
+++ api/controllers/serverController.js
@@ -1,12 +1,15 @@
-import { guildSettingsUpdate, guildSettingsAdd } from '#services'
+import { guildSettingsUpdate, guildSettingsAdd, guildSettingsRemove } from '#services'
 import { Action } from '#base'
 
 const GuildSettingsUpdate = (req, res)=> new Action(req, res, {service: guildSettingsUpdate, sync: true})
 const GuildSettingsAdd = (req, res)=> new Action(req, res, {service: guildSettingsAdd, sync: true})
 
+const GuildSettingsRemove = (req, res)=> new Action(req, res, {service: guildSettingsRemove, sync: true})
+
 
 export {
 	
 	GuildSettingsUpdate,
-	GuildSettingsAdd
+	GuildSettingsAdd,
+	GuildSettingsRemove
 }
