9e95639e412ec3c2146fd56329bc7e5a42beeead
burak behlül <burakyabgu@gmail.com>
2025-10-27 00:25:51 +0300

    refactor: Struct routes

--- api/routers/serverRoute.js
+++ api/routers/serverRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { GuildSettingsAdd, GuildSettingsUpdate } from '#controllers';
+import { GuildSettingsAdd, GuildSettingsUpdate, GuildSettingsRemove } from '#controllers';
 
 const router = express.Router();
 
 router.get('/', (req,res)=> res.send('server route'))
 
-router.post('/server/:guildId', GuildSettingsAdd);
+router.post('/server/penal/:guildId', GuildSettingsAdd);
+router.delete('/server/penal/:guildId', GuildSettingsRemove);
+
 router.put('/server/:guildId', GuildSettingsUpdate);
 
 
